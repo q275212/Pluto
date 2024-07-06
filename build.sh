3d6fb186fdcd72524c31b335b10a1c5bc7c8f4b4
q275212 <41291979+q275212@users.noreply.github.com>
2024-07-06 18:27:29 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ mkdir -p build
 cmake -G Ninja -S . -B build \
       -DCMAKE_C_COMPILER="gcc" \
       -DCMAKE_CXX_COMPILER="g++" \
+      -DLLVM_TARGETS_TO_BUILD="X86;AArch64"
       -DCMAKE_INSTALL_PREFIX="install" \
       -DCMAKE_BUILD_TYPE=Release
 ninja -j`nproc` -C build install
